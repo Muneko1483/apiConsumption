2a3a77a1492e18553c471115ee2f0addff3507cd
Guzman <juan.guzman@accenture.com>
2018-09-07 11:33:53 -0500

    jalo

--- apiConsumption.js
+++ apiConsumption.js
@@ -1,29 +1,29 @@
-var Request = require("request");
+var request = require("request");
     express= require("express");
     app = express();
     const port = process.env.PORT || 5656
-    const htt = "http://localhost:3000/users"
-Request.get(htt, (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
+    
+
+app.get('/users',(req,res) =>{
+    request.get("http://localhost:3000/users",(error,response,body) => {
+        res.status(200).send(JSON.parse(body));
+        console.log(JSON.parse(body));
+})
 });
 
-Request.post({
-    "headers": { "content-type": "application/json" },
-    "url": "http://localhost:3000/users",
-    "body": JSON.stringify({
-        "name": "Eliza",
-        "age": "26",
-        "email" : "eliza_lll@hotmail.com"
+app.post('/users',(req,res) =>{
+    request.post({url :'http://localhost:3000/users',
+    "body": JSON.parse(body)({
+            name:'Tadeo',
+            age : '34',
+            email :'tadeo@jdjdj'
+        })
+     },(error, response, body) => {
+        res.status(200).send(JSON.parse(body));
+        console.dir(JSON.parse(body));
     })
-}, (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
-});
+      });
+  
 app.listen(port);
 
 
